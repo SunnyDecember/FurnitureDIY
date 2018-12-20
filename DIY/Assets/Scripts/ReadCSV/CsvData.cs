5a9c03249d610e3f2fd8169d24be6ab7d505ac08
HappinessBrother <mrliukeepmove@163.com>
2018-12-20 23:54:47 +0800

    根据分类拿表数据

---
+++ DIY/Assets/Scripts/ReadCSV/CsvData.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CsvData : MonoBehaviour
+{
+
+    //public List<ModelInfo> ModelsInfo;
+    //按分类来存模型数据
+    Dictionary<string, List<ModelInfo>> categoryModelInfo = new Dictionary<string, List<ModelInfo>>();
+
+    void Start()
+    {
+        //拿到表数据
+        var ModelsInfo = ReadCsv.Load();
+        //对表数据的模型按类型分类
+        for (int i = 0; i < ModelsInfo.Count; i++)
+        {
+            string categoryatalogName = ModelsInfo[i].Category;
+            List<ModelInfo> tempList;
+            //判断分类是否存在，存在则在原基础上添加，不存在则创建，然后Add
+            if (categoryModelInfo.ContainsKey(categoryatalogName))
+            {
+                tempList = categoryModelInfo[categoryatalogName];
+                tempList.Add(ModelsInfo[i]);
+            }
+            else
+            {
+                tempList = new List<ModelInfo>();
+                tempList.Add(ModelsInfo[i]);
+                categoryModelInfo.Add(categoryatalogName, tempList);
+            }
+            
+        }
+    }
+
+    //通过catalog对表格物体按类型分类
+    public List<ModelInfo> GetCategoryModels(string categoryName)
+    {
+        if (categoryModelInfo.ContainsKey(categoryName))
+        {
+            return categoryModelInfo[categoryName];
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
+
---
+++ DIY/Assets/Scripts/ReadCSV/ReadCsv.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Data;
+using Excel;
+using System.IO;
+using System.Collections.Generic;
+
+public struct ModelInfo
+{
+    public string Category;
+    public string Name;
+    public string ImageLocalPos;
+    public string ModelLocalPos;
+};
+
+public class ReadCsv
+{
+    private static string excelPath = "F:/WorkProject/PlayAnimation/Assets/Resources/Data/模型数据.xls";
+    //public List<ModelInfo> ModelsInfo;
+    //void Start()
+    //{
+    //    //Debug.Log(Application.dataPath);
+    //    //string realyPath = Path.Combine(Application.dataPath, excelPath);
+    //    ModelsInfo = Load(excelPath);
+    //    if (null != ModelsInfo)
+    //    {
+    //        foreach (ModelInfo data in ModelsInfo)
+    //        {
+    //            Debug.Log(string.Format("<color=red>{0},{1},{2},{3}</color>", data.Category, data.Name, data.ImageLocalPos, data.ModelLocalPos));
+    //        }
+    //    }
+    //}
+    
+
+    static DataSet ReadExcel()
+    {
+       
+        FileStream stream = File.Open(excelPath, FileMode.Open, FileAccess.Read, FileShare.Read);
+        IExcelDataReader excelReader = null;
+        if (excelPath.ToLower().EndsWith(".xlsx"))
+        {
+            excelReader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+        }
+        else if (excelPath.ToLower().EndsWith(".xls"))
+        {
+            excelReader = ExcelReaderFactory.CreateBinaryReader(stream);
+        }
+
+        DataSet result = excelReader.AsDataSet();
+        return result;
+    }
+
+    public static List<ModelInfo> Load()
+    {
+        List<ModelInfo> _data = new List<ModelInfo>();
+        DataSet resultds = ReadExcel();
+        int columns = resultds.Tables[0].Columns.Count;
+        int rows = resultds.Tables[0].Rows.Count;
+        for (int i = 1; i < rows; i++)
+        {
+            ModelInfo temp_data = new ModelInfo();
+            temp_data.Category = resultds.Tables[0].Rows[i][0].ToString();
+            temp_data.Name = resultds.Tables[0].Rows[i][1].ToString();
+            temp_data.ImageLocalPos = resultds.Tables[0].Rows[i][2].ToString();
+            temp_data.ModelLocalPos = resultds.Tables[0].Rows[i][3].ToString();
+            _data.Add(temp_data);
+        }
+
+        return _data;
+    }
+}
